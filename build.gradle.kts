2ec983b34f17c5564ae4c3c4b3993076469fd7c8
connorhartley <vectrixu@gmail.com>
2025-07-08 11:17:24 +1200

    update publishing url

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,13 @@ plugins {
 
 // Project metadata is configured in gradle.properties
 
-indraSonatype {
-  useAlternateSonatypeOSSHost("s01")
+nexusPublishing {
+  repositories {
+    sonatype {
+      nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+      snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+      username.set(findProperty("sonatypeUsername") as String)
+      password.set(findProperty("sonatypePassword") as String)
+    }
+  }
 }
