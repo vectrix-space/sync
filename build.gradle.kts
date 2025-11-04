dcdc52240c0d778850d997bf275102253e7fa266
connorhartley <vectrixu@gmail.com>
2025-11-04 13:02:47 +1300

    chore(build): remove nexus overrides that are no longer needed

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,3 @@ plugins {
 }
 
 // Project metadata is configured in gradle.properties
-
-nexusPublishing {
-  val sonatypeUsername: String = (findProperty("sonatypeUsername") as? String) ?: ""
-  val sonatypePassword: String = (findProperty("sonatypePassword") as? String) ?: ""
-
-  repositories {
-    named("sonatype") {
-      nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
-      snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
-      username.set(sonatypeUsername)
-      password.set(sonatypePassword)
-    }
-  }
-}
